227c4a2f80f73eabd2108e2f1b427739bd884614
Ilan Rojas <bobatthevelvetroom@gmail.com>
2023-03-28 15:23:42 -0400

    Version 2:C - Fixed images not loading properly

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
-// write your code here
-
-//--------MENU ALREADY---------------
-
 let ramenName = document.querySelector(".name")
 let detailImg = document.querySelector(".detail-image")
 let ramenMenu = document.querySelector("#ramen-menu")
 let ramenComment = document.querySelector("#comment-display")
 let ramenRestaurant = document.querySelector('.restaurant')
 let ratingDisplay = document.querySelector('#rating-display')
-
-//====================================================
 let ramenForm = document.querySelector("#new-ramen")
+
 document.addEventListener('DOMContentLoaded', () => {
    fetch("http://localhost:3000/ramens")
         .then((res) => res.json())
         .then((data) =>
         {data.forEach(function (ramen) {
-            console.log(ramen)
-
         })
         renderRamen(data)
     })
@@ -27,16 +20,14 @@ ramenForm.addEventListener('submit', (e) => {
     e.preventDefault()
     let ramen = {}
     ramen = {
-
         name : e.target.name.value,
         restaurant : e.target.restaurant.value,
         image : e.target.image.value,
         rating : e.target.rating.value,
         comment : e.target["new-comment"].value
     }
-
+    postRamen(ramen)
     renderRamen([ramen])
-
 })
 
 function renderRamen(ramens){
@@ -45,7 +36,7 @@ function renderRamen(ramens){
         newRamen.src = ramen.image;
 
         newRamen.addEventListener('click', () => {
-            detailImg.src = newRamen.image
+            detailImg.src = ramen.image
             ratingDisplay.textContent = `${ramen.rating}`
             ramenComment.textContent = ramen.comment
             ramenRestaurant.textContent = ramen.restaurant
@@ -53,4 +44,19 @@ function renderRamen(ramens){
         })
         ramenMenu.append(newRamen)
    })
+}
+
+function postRamen(ramen){
+    fetch("http://localhost:3000/ramens", {
+        method: "POST",
+        headers: {
+            "content-type": "Application/json",
+        },
+        body: JSON.stringify(ramen),
+    })
+        .then((res) => res.json())
+        .then((data) => renderRamen([data]))
+        .catch(function(error) {
+            console.log(" OHHH NOOOOOOOO, anyways heres the error,",error)
+        })
 }
